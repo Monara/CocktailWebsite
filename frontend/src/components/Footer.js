e2427e5326f8de68fb334a932f23bcd8febd7d43
Monika <monika.radaviciute@gmail.com>
2022-03-02 02:25:38 +0200

    Added media queries for responsiveness

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,8 +7,8 @@ function Footer() {
   return (
     <div className='footer-container'>
         <div className='text-container'>
-        <p>&#169; TheCocktailParty 2022.</p> 
-        <p><a href="mailto:thecocktailparty@pm.me?subject=New message for TheCocktailParty">thecocktailparty@pm.me</a></p>
+        <p className="small-par">&#169; TheCocktailParty 2022.</p> 
+        <p className="small-par"><a href="mailto:thecocktailparty@pm.me?subject=New message for TheCocktailParty">thecocktailparty@pm.me</a></p>
         </div>      
     </div>
   )
